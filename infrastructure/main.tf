477dcea928ea08d37c33259cef8678ebb2465cae
Eric Humphries <ezhumphries@gmail.com>
2021-03-09 21:15:18 -0800

    fixed type

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -28,11 +28,8 @@ locals {
 resource "aws_instance" "photogallery-service" {
   ami                    = var.AMI_ID
   instance_type          = var.INSTANCE_TYPE
-  # the VPC subnet
   subnet_id              = element(module.main-vpc.public_subnets, 0)
-  # the security group
   vpc_security_group_ids = [aws_security_group.photogallery_service_securitygroup.id]
-  # the public SSH key
   key_name               = aws_key_pair.mykeypair.key_name
   tags                   = local.common_tags
 }
