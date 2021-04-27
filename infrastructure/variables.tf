a9abc9a208bfa0e0cb34584502e2c59e72bf39d7
Eric Humphries <ezhumphries@gmail.com>
2021-04-27 13:45:22 -0700

    tweakend packer template

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -24,6 +24,10 @@ variable "PATH_TO_PUBLIC_KEY" {
   default = "mykey.pub"
 }
 
+# variable "PATH_TO_PRIVATE_KEY" {
+#   default = "mykey"
+# }
+
 variable "INSTANCE_TYPE" {
   default = "t2.micro"
 }
