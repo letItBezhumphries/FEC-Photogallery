62e76ddcd8cbd3fbb5d1c7f6c5d2835e0c8e08f0
Eric Humphries <ezhumphries@gmail.com>
2021-09-03 15:01:37 -0700

    fixed issues with test-server file

--- server/test-server.js
+++ server/test-server.js
@@ -1,14 +1,6 @@
-require('dotenv').config();
 const express = require('express');
-const connectDB = require('../database/index');
-// const path = require('path');
-// const cors = require('cors');
 const app = express();
-// const DIST_DIR = path.join(__dirname, '../public/dist');
 const PORT = process.env.PORT || 3003;
-
-// Connect Database
-connectDB();
 
 // Init Middleware
 app.use(
